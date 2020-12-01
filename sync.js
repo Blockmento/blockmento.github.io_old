1274277b819d4d405ce61e5aa7640cf07558e34a
Blockmento <75256857+Blockmento@users.noreply.github.com>
2020-12-01 16:46:13 +0100

    bugfix

--- sync.js
+++ sync.js
@@ -2,7 +2,7 @@ async function registerPeriodicNewsCheck() {
   const registration = await navigator.serviceWorker.ready;
   try {
     await registration.periodicSync.register('fetch-news', {
-      minInterval= 0,
+      minInterval= 0;
     });
   } catch {
     console.log('Periodic Sync could not be registered!');

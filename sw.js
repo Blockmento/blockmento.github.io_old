7a97aa32ee19a72acc987e8c1f992cde425b036c
Blockmento <75256857+Blockmento@users.noreply.github.com>
2020-12-02 17:34:38 +0100

    bugfix

--- sw.js
+++ sw.js
@@ -60,7 +60,7 @@ function getTime(){
   var today=new Date(time.getTime() - (time.getTimezoneOffset() * 60000)).toISOString().replace(/T/, ' ').replace(/\..+/, ''); //sets the Time to the users timezone
   var Datum = today.slice(0,10).replace(/-/g,"."); //gets only the Parts for Date/Time
   var Uhrzeit = today.slice(11,19);
-  return {Datum, Uhrzeit,}
+  return {Datum, Uhrzeit,};
 }
 
 function Change_N() {
@@ -70,9 +70,9 @@ function Change_N() {
 
 navigator.connection.addEventListener('typechange', Change_N);
 
-//navigator.getBattery.then(function (battery) { //opens stream to Battery API
+/*navigator.getBattery.then(function (battery) { //opens stream to Battery API
 
-/*function Change_B() {
+function Change_B() {
   console.log("change");
   saveAkku(battery.level, battery.charging);
   }

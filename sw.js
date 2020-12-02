6d4c6493fa16ea47ecad65212d4b70310b89ba42
Blockmento <75256857+Blockmento@users.noreply.github.com>
2020-12-02 17:01:36 +0100

    removed sync and addet network and akku to sw

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+import("./DB.js");
+
 //https://developers.google.com/web/fundamentals/primers/service-workers
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
@@ -30,4 +32,29 @@ self.addEventListener('fetch', function(event) {
       }
     )
   );
+});
+
+function Change() {
+  console.log("change");
+  saveNetwork(navigator.connection.type, navigator.onLine);
+}
+
+navigator.connection.addEventListener('typechange', Change);
+
+navigator.getBattery().then(function (battery) { //opens stream to Battery API
+
+function Change() {
+  console.log("change");
+  saveAkku(battery.level, battery.charging);
+  }
+
+battery.addEventListener('chargingchange', Change);
+battery.addEventListener('levelchange', Change);
+
+console.log(battery.level);
+
+//store.clear();
+
+//saveAkku(battery.level, battery.charging)
+
 });

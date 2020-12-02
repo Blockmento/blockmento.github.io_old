51dee6a748247f2c7ff41caf889e7c3cb1bb4758
Blockmento <75256857+Blockmento@users.noreply.github.com>
2020-12-02 17:21:08 +0100

    put everithing in sw

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-import("./DB.js");
-
 //https://developers.google.com/web/fundamentals/primers/service-workers
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
@@ -34,6 +32,15 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+
+function getTime(){
+  var time=new Date(); //gats the Date and Time in utc
+  var today=new Date(time.getTime() - (time.getTimezoneOffset() * 60000)).toISOString().replace(/T/, ' ').replace(/\..+/, ''); //sets the Time to the users timezone
+  var Datum = today.slice(0,10).replace(/-/g,"."); //gets only the Parts for Date/Time
+  var Uhrzeit = today.slice(11,19);
+  return {Datum, Uhrzeit,}
+}
+
 function Change() {
   console.log("change");
   saveNetwork(navigator.connection.type, navigator.onLine);
@@ -58,3 +65,25 @@ console.log(battery.level);
 //saveAkku(battery.level, battery.charging)
 
 });
+
+function saveAkku(level, state) {
+  var db_akku = indexedDB.open("Daten");
+  db_akku.onsuccess = function(){
+    console.log("Speichere Akku");
+    var db = db_akku.result;
+    var tx = db.transaction("Akku", "readwrite");
+    var store = tx.objectStore("Akku");
+    store.put({date: getTime().Datum, time: getTime().Uhrzeit, level: level , state: state});
+  };
+}
+
+function saveNetwork(type, state) {
+  var db_akku = indexedDB.open("Daten");
+  db_akku.onsuccess = function(){
+    console.log("Speichere Netzwerk");
+    var db = db_akku.result;
+    var tx = db.transaction("Network", "readwrite");
+    var store = tx.objectStore("Network");
+    store.put({date: getTime().Datum, time: getTime().Uhrzeit, type: type , state: state});
+  };
+}

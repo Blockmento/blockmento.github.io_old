8d89b628b2077a563194d8f03474442f0d94df1e
Blockmento <75256857+Blockmento@users.noreply.github.com>
2020-12-03 20:56:06 +0100

    cleand up

--- sw.js
+++ sw.js
@@ -31,36 +31,3 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
-
-self.addEventListener('sync', event => {
-  if (event.tag === 'test') {
-    event.waitUntil(serverSync());
-  }
-});
-
-async function serverSync() {
-  console.log("initilaced sync");
-  function Change() {
-      console.log("sync");
-      saveNetwork(navigator.connection.type, navigator.onLine);
-    }
-    function saveNetwork(type, state) {
-      var db_network = indexedDB.open("Daten");
-      db_network.onsuccess = function(){
-        console.log("Speichere Netzwerk");
-        var db = db_network.result;
-        var tx = db.transaction("Network", "readwrite");
-        var store = tx.objectStore("Network");
-        store.put({date: getTime().Datum, time: getTime().Uhrzeit, type: type , state: state});
-      };
-    }
-
-    navigator.connection.addEventListener('typechange', Change);
-
-
-    //console.log("sync");
-    //saveNetwork(navigator.connection.type, navigator.onLine);
-
-
-  //navigator.connection.addEventListener('typechange', Change);
-}

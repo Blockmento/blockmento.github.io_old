a097b4c2b5495feae75292997fd834618e210f44
Blockmento <75256857+Blockmento@users.noreply.github.com>
2020-12-13 10:57:32 +0100

    testing background

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-function Test() {
-  console.log("Test");
-}
-
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
     "id": "sampleContextMenu",
     "title": "Sample Context Menu",
     "contexts": ["selection"]
   });
+});
+
+// This will run when a bookmark is created.
+chrome.bookmarks.onCreated.addListener(function() {
+  // do something
+  console.log("test");
 });
